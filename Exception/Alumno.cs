14f92163bf6a3de9be27ed9289cf806e00a59558
Hlorenzoo <hernanlorenzo97@hotmail.com>
2023-01-16 13:16:25 -0300

    Agregar archivos de proyecto.

---
+++ Exception/Alumno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptionn
+{
+    public class Alumno
+    {
+     public Alumno(string nombre) 
+        {
+            string name = nombre.Trim();
+            if(name.Length <1 || name.Length > 50)
+            {
+                throw new Exception("Tiene que poner un nombre valido");
+            }
+            Nombre = name;
+        }
+           public string Nombre { get; set; }
+        
+        }
+
+
+    }
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿
+using Exceptionn;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Alumno alumno = new Alumno("");
+
+                MessageBox.Show("Alumno Creado");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
